e81b1c42d60c198904f791dfa8d862ecf93731aa
Clément Hurel <clement.hurel@gmail.com>
2019-10-17 14:04:09 -0600

    Fix fatal error when duplicate is not a string

--- src/Implementation/Formatters/UniqueItems.php
+++ src/Implementation/Formatters/UniqueItems.php
@@ -10,8 +10,16 @@ class UniqueItems extends Formatter
 
     public function replacements(): array
     {
-        return [
-            ':duplicate:' => $this->error->keywordArgs()['duplicate']
-        ];
+    	if (is_string($this->error->keywordArgs()['duplicate'])) {
+            return [
+                ':duplicate:' => $this->error->keywordArgs()['duplicate']
+            ];
+        } elseif (json_encode($this->error->keywordArgs()['duplicate'])) {
+            return [
+                ':duplicate:' => json_encode($this->error->keywordArgs()['duplicate'])
+            ];
+        } else {
+            return [':duplicate:' => ''];
+        }
     }
 }
