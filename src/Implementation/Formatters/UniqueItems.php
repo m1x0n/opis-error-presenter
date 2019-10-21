00fa6b62d549f68eb6f95f486464129d3b20482c
Clément Hurel <clement.hurel@gmail.com>
2019-10-21 07:32:27 -0600

    Fixing github autocommit

--- src/Implementation/Formatters/UniqueItems.php
+++ src/Implementation/Formatters/UniqueItems.php
@@ -9,22 +9,11 @@ class UniqueItems extends Formatter
     public const MESSAGE = "The attribute contains duplicated item: ':duplicate:'.";
 
     public function replacements(): array
+    {
         $duplicate = $this->error->keywordArgs()['duplicate'];
         $duplicate = json_encode($duplicate);
-        
         return [
             ':duplicate:' => $duplicate
         ];
-        if (is_string($this->error->keywordArgs()['duplicate'])) {
-            return [
-                ':duplicate:' => $this->error->keywordArgs()['duplicate']
-            ];
-        } elseif (json_encode($this->error->keywordArgs()['duplicate'])) {
-            return [
-                ':duplicate:' => json_encode($this->error->keywordArgs()['duplicate'])
-            ];
-        } else {
-            return [':duplicate:' => ''];
-        }
     }
 }
